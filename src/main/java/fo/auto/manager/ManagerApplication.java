8bd555ac3b5d086eb28bd77dc3f07ddebf4888da
OlehFliurkevych <oleh0809@gmail.com>
2018-09-19 17:26:42 +0300

    Complete project

--- src/main/java/fo/auto/manager/ManagerApplication.java
+++ src/main/java/fo/auto/manager/ManagerApplication.java
@@ -2,11 +2,15 @@ package fo.auto.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @SpringBootApplication
-public class ManagerApplication {
+public class ManagerApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
+
+
 }
